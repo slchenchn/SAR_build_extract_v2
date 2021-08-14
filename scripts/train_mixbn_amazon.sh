bdf242d56e9bd601f8b3d773130bd590568fbf47
ChengShuaiLin <1106982578@qq.com>
2021-08-14 21:29:03 +0800

    none

--- scripts/train_mixbn_amazon.sh
+++ scripts/train_mixbn_amazon.sh
@@ -4,12 +4,18 @@ cd /home/csl/code/SAR_build_extract_v2
 # conda activate sar_build_extract_v2
 
 # floating point arithmetic operations are not support by vanilla bash
-# for rt in {0..10..2}
-# for rt in 0
-# for rt in 0 0.25 0.5 0.75 1
+
+# whole model
+# for rt in 0.25 0.5 0.75 
+# do 
+# # echo $((rt/10.))
+# python tools/train.py configs/mix_bn/deeplabv3plus_512x512_4k_mixbn_rs2_to_gf3_2_attach.py --options {model.backbone.norm_cfg.ratio=$rt,model.decode_head.norm_cfg.ratio=$rt,model.auxiliary_head.norm_cfg.ratio=$rt}
+# done
+
+# only backbone
 for rt in 0 0.25 0.5 0.75 
 do 
 # echo $((rt/10.))
-python tools/train.py configs/mix_bn/deeplabv3plus_512x512_4k_mixbn_rs2_to_gf3_1_attach.py --options {model.backbone.norm_cfg.ratio=$rt,model.decode_head.norm_cfg.ratio=$rt,model.auxiliary_head.norm_cfg.ratio=$rt}
+python tools/train.py configs/mix_bn/deeplabv3plus_512x512_4k_mixbn_rs2_to_gf3_0_attach_backbone.py --options model.backbone.norm_cfg.ratio=$rt
 done
 
