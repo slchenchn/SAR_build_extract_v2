3767c2caa7d3b68510ad14723173dc0a4fd7a28f
chenda <1106982578@qq.com>
2021-09-04 14:28:21 +0800

    n

--- configs/_base_/datasets/sar_building_rs2_hoekman.py
+++ configs/_base_/datasets/sar_building_rs2_hoekman.py
@@ -17,7 +17,7 @@ train_pipeline = [
     dict(type='Collect', keys=['img', 'gt_semantic_seg'])
 ]
 test_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadNpyFromFile'),
     dict(
         type='MultiScaleFlipAug',
         img_scale=(512, 512),
